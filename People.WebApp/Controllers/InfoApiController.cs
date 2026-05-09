0b6ea6756a0e274d39dba47aee038f797e133b13
Varun Sharma <25815202+vsha041@users.noreply.github.com>
2026-05-10 11:19:52 +1200

    Added endpoint to getch POD name.

---
+++ People.WebApp/Controllers/InfoApiController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace People.WebApp.Controllers
+{
+    [Route("api/info")]
+    [ApiController]
+    public class InfoApiController : ControllerBase
+    {
+        [HttpGet]
+        public string GetMachineName()
+        {
+            return $"The request was processed by {Environment.MachineName}";
+        }
+    }
+}
